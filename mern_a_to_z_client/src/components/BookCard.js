a50f4d5f82746a63a69172b944aa0f768a805b0f
jenniferty <bqd3000@autuni.ac.nz>
2022-08-19 19:02:15 +1200

    Add all files

---
+++ mern_a_to_z_client/src/components/BookCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+
+const BookCard = (props) => {
+    const  book  = props.book;
+
+    return(
+        <div className="card-container">
+            <img src="https://static-ppimages.freetls.fastly.net/nielsens/9781910974254.jpg?canvas=363,600&fit=bounds&height=600&mode=max&width=363&404=default.jpg" alt="" />
+            <div className="desc">
+                <h2>
+                    <Link to={`/show-book/${book._id}`}>
+                        { book.title }
+                    </Link>
+                </h2>
+                <h3>{book.author}</h3>
+                <p>{book.description}</p>
+            </div>
+        </div>
+    )
+};
+
+export default BookCard;
